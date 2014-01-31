cfaac618a31f8a223e7220a07a067e2c1bb5ad65
Siva <ksivamuthu@ceiamerica.com>
2014-01-31 15:39:52 -0500

    Committed working phonegap project

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,51 +3,37 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-compress');
     grunt.loadNpmTasks('grunt-phonegap-build');
     
-     var yeomanConfig = {
-        app: 'www',
-        dist: 'dist'
-    };
-
-    var phonegapBuildConfig = {
-        debug: {
-            options: {
-                archive: 'app.zip',
-                'appId': 'XXXXXX',
-                'user': {
-                    'email': 'myemail@server.com',
-                    'password': 'password1234'
-                },
-                keys: {
-                    //ios: { 'password': password1234' },
-                    android: { 'key_pw': 'password1234', 'keystore_pw': 'password1234' },
-                    blackberry: { 'password': 'password1234'}
-                },
-                download: {
-                    ios: 'dist/ios.ipa',
-                    android: 'dist/android.apk',
-                    blackberry: 'dist/blackberry.ota',
-                    winphone: 'dist/winphone.xap'
-                }
-            }
-        }};
-
-    var compressConfig = {
-        main: {
-            options: {
-                archive: 'app.zip'
-            },
-            expand: true,
-            cwd: 'www/',
-            src: ['*.html', 'css/**/*.css', 'js/**/*.js', 'config.xml', 'img/**/*'],
-        }
-    };
-
     grunt.initConfig({
-        'phonegap-build': phonegapBuildConfig,
-        compress: compressConfig
+        'phonegap-build' : {
+                options: {
+                    archive: 'app.zip',
+                    'appId': 'someid',
+                    'user': {
+                        'email': 'somemail@gmail.com',
+                        'password': 'somepassword$'
+                    },
+                    download: {
+                        android: 'releases/android.apk',
+                        winphone: 'releases/winphone.xap'
+                    },
+                },
+                debug: {
+
+                }
+        },
+        'compress' : {
+            main: {
+                options: {
+                    archive: 'app.zip'
+                },
+                expand: true,
+                cwd: 'www/',
+                src: ['*.html', 'css/**/*.css', 'js/**/*.js', 'config.xml', 'img/**/*'],
+            }    
+        }
     });
 
-    grunt.registerTask('remote_build', [
+    grunt.registerTask('build', [
         'compress',
         'phonegap-build:debug'
     ]);
